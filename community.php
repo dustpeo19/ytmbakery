cb6bbbb876924cbbce5b396df2eae465340ab9e1
dustpeo19 <sm18570@naver.com>
2020-12-02 03:26:17 +0900

    fontawesome address changed

--- community.php
+++ community.php
@@ -68,7 +68,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/5.3.1/css/font-awesome.min.css">
 	<link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico"/>
 	<link rel="stylesheet" href="css/main.css">
-	<script src="https://kit.fontawesome.com/b8f1cadba4.js" crossorigin="anonymous"></script>
+	<script src="https://kit.fontawesome.com/50d723c333.js" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<style>
